a335609b17449b9946061ad2ed3d5a45e23f8ecc
Richard Ranke <richard_ranke@homedepot.com>
2021-10-13 09:44:56 -0400

    initial commit

---
+++ apps/ap/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-patterns-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ap';
+}
